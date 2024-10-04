e3ff9a4a798bf2cbf01d25f7aff6869a15d47e1e
Daniel Williams <danieljameswillliams2016@gmail.com>
2024-10-04 21:25:05 +0100

    Added NavigationNode class.

---
+++ Assets/Scripts/NavigationNode.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public abstract class NavigationNode : MonoBehaviour
+{ 
+    [SerializeField] protected List<NavigationNode> linkedNodes;
+
+    public abstract NavigationNode GetNextNode();
+}
