d76b2f6020062397a7a49d86e8ceec29807f65b0
Daniel Williams <danieljameswillliams2016@gmail.com>
2024-10-04 21:34:41 +0100

    Added FixedNavigationNode implementation

---
+++ Assets/Scripts/Navigation Nodes/FixedNavigationNode.cs
@@ -0,0 +1,7 @@
+﻿public class FixedNavigationNode : NavigationNode
+{
+    public override NavigationNode GetNextNode()
+    {
+        return linkedNodes.Count > 0 ? linkedNodes[0] : null;
+    }
+}
