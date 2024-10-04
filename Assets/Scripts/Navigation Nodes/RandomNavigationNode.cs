ecb99eb37ce1857f7a71dd67655dd929b70ff98f
Daniel Williams <danieljameswillliams2016@gmail.com>
2024-10-04 21:46:08 +0100

    Added RandomNavigationNode and set up waypoints

---
+++ Assets/Scripts/Navigation Nodes/RandomNavigationNode.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class RandomNavigationNode : NavigationNode
+{
+    public override NavigationNode GetNextNode()
+    {
+        if (linkedNodes.Count > 0)
+        {
+            var index = Random.Range(0, linkedNodes.Count);
+            return linkedNodes[index];
+        }
+
+        return null;
+    }
+}
