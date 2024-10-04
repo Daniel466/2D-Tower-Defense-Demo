81c524aa391083be80b4bb50b72d88085ff5ee07
Daniel Williams <danieljameswillliams2016@gmail.com>
2024-10-04 22:20:44 +0100

    Added AutoMovement behaviour.

---
+++ Assets/Scripts/AutoMovement.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Assertions;
+
+public class AutoMovement : MonoBehaviour
+{
+    [SerializeField] private NavigationNode startingNode;
+    [SerializeField] private float speed = 1.0f;
+
+    private NavigationNode targetNode;
+
+    private void Awake()
+    {
+        Assert.IsNotNull(startingNode);
+    }
+
+    private void Start()
+    {
+        targetNode = startingNode;
+    }
+
+    private void Update()
+    {
+        if (targetNode != null)
+        {
+            var currentPosition = transform.position;
+            var targetPosition = targetNode.gameObject.transform.position;
+            transform.position = Vector2.MoveTowards(currentPosition, targetPosition, speed);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject == targetNode.gameObject)
+        {
+            targetNode = targetNode.GetNextNode();
+        }
+    }
+}
