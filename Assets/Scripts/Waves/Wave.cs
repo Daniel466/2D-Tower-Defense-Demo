7a91fa56cfe563db7b4ec4a93dc010bf396ba041
Daniel Williams <danieljameswillliams2016@gmail.com>
2024-10-05 15:24:26 +0100

    Added Wave parent class.

---
+++ Assets/Scripts/Waves/Wave.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+public abstract class Wave : PausableTimer
+{
+    protected int currentInstructionIndex;
+
+    public event EventHandler<EventArgTemplate<bool>> WaveCompleted;
+
+    public virtual void BeginWave()
+    {
+        CompleteNextInstruction();
+    }
+
+    public virtual void HandleCompletion(bool success)
+    {
+        if (WaveCompleted != null)
+        {
+            var completion = new EventArgTemplate<bool>(success);
+            WaveCompleted.Invoke(this, completion);
+        }
+    }
+
+    protected abstract void CompleteNextInstruction();
+}
