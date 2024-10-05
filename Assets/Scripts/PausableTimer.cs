daf2ffe428d10eb9dd7bda522ca9dd42f1594c07
Daniel Williams <danieljameswillliams2016@gmail.com>
2024-10-05 14:10:10 +0100

    Created The PausableTimer Class

---
+++ Assets/Scripts/PausableTimer.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class PausableTimer : MonoBehaviour
+{
+    public float Timer { get; private set; }
+    
+    protected void Start()
+    {
+       ResetTimer();
+    }
+
+    protected void Update()
+    {
+        Timer += Time.deltaTime;
+    }
+
+    protected void ResetTimer()
+    {
+        Timer = 0.0f;
+    }
+}
