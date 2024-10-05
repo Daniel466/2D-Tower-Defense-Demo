ba1c9531e6f4e102cd944bc71014436551d7388a
Daniel Williams <danieljameswillliams2016@gmail.com>
2024-10-05 19:04:53 +0100

    Added Tower Node.

---
+++ Assets/Scripts/TowerNode.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(BoxCollider2D))]
+public class TowerNode : MonoBehaviour
+{
+    // TODO: Tower Selection UI
+
+    private GameObject placedTower;
+
+    public GameObject PlacedTower
+    {
+        get { return placedTower; }
+    }
+
+    private void Awake()
+    {
+        // TODO: Assert -- Tower Selection UI != null
+    }
+
+    private void Start()
+    {
+        
+    }
+
+    private void ToggleSelectionUI()
+    {
+        
+    }
+
+    private void OnTowerPurchased(object sender, EventArgTemplate<TowerConfiguration> purchasedTower)
+    {
+        if (placedTower != null)
+        {
+            Destroy(placedTower);
+        }
+
+        var prefab = purchasedTower.Attachment.towerPrefab;
+        placedTower = Instantiate(prefab);
+        placedTower.transform.parent = transform;
+        placedTower.transform.localPosition = new Vector3(0, 0);
+        OnCloseRequested();
+    }
+
+    public void OnCloseRequested()
+    {
+        
+    }
+
+    public void OnNodeSelected()
+    {
+        ToggleSelectionUI();
+    }
+}
