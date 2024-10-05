cb7e4969e718bde41352ebd7a851da85581f7a06
Daniel Williams <danieljameswillliams2016@gmail.com>
2024-10-05 14:37:30 +0100

    Create The EventArgTemplate

---
+++ Assets/Scripts/EventArgTemplate.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+public class EventArgTemplate<T> : EventArgs
+{
+    public T Attachment { get; private set; }
+
+    public EventArgTemplate(T attachment)
+    {
+        Attachment = attachment;
+    }
+}
