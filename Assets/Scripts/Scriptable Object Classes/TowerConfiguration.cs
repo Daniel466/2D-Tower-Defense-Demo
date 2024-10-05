a11b48b8c822c04716b3a31771d8638240eadc15
Daniel Williams <danieljameswillliams2016@gmail.com>
2024-10-05 18:26:29 +0100

    Added tower prefabs and tower configurations

---
+++ Assets/Scripts/Scriptable Object Classes/TowerConfiguration.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName = "TowerConfiguration.asset", 
+                menuName = "TowerDefense/Tower Configuration", 
+                order = 2)]
+public class TowerConfiguration : ScriptableObject
+{
+    public Sprite towerImage;
+    public int price;
+    public GameObject towerPrefab;
+}
