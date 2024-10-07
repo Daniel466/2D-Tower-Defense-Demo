b72bf0ec007c527794a3a4a69acf97d7fe2f9574
Daniel Williams <danieljameswillliams2016@gmail.com>
2024-10-07 15:35:28 +0100

    Added Targetable Behaviour and updated skeletons with targetable and damageable

---
+++ Assets/Scripts/Action Framework/Targetable.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using UnityEngine;
+using System.Collections.Generic;
+
+public class Targetable : MonoBehaviour
+{
+    [SerializeField] private List<TargetType> types = new List<TargetType>();
+
+    public event Action TargetableStatusRemoved;
+
+    public void Disable()
+    {
+        SafeEventHandler.SafelyBroadcastAction(ref TargetableStatusRemoved);
+        enabled = false;
+    }
+}
