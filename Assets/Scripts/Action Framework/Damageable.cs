a5be59fd05d06798a303c4b7d8109f673025c3dd
Daniel Williams <danieljameswillliams2016@gmail.com>
2024-10-07 14:12:05 +0100

    Added Damageable Behaviour

---
+++ Assets/Scripts/Action Framework/Damageable.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System;
+using System.Collections.Generic;
+
+public class Damageable : MonoBehaviour
+{
+    [SerializeField] private int maxHealth = 10;
+    [SerializeField] private List<string> damagingTags = new List<string>();
+    
+    public int CurrentHealth { get; private set; }
+    public int MaxHealth { get { return maxHealth; } }
+
+    public event Action DamageTaken; 
+
+    private void Awake()
+    {
+        CurrentHealth = MaxHealth;
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        HandleDamage(other);
+    }
+
+    private void HandleDamage(Collider2D other)
+    {
+        var validTag = damagingTags.Contains(other.gameObject.tag);
+        if (!validTag)
+        {
+            return;
+        }
+        
+        // TODO: Implement Damager/projectile/ect. handling
+    }
+
+    private void TakeDamage(int damageAmount)
+    {
+        CurrentHealth = Mathf.Max(0, CurrentHealth - damageAmount);
+        SafeEventHandler.SafelyBroadcastAction(ref DamageTaken);
+    }
+}
